c7f1f9f72988bb2e68879d6475f200fcc90ab8bb
AntoColu <antonioby99@hotmail.it>
2023-08-20 12:09:58 +0200

    Update catalogo.blade.php

--- resources/views/public/catalogo.blade.php
+++ resources/views/public/catalogo.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.footer')
+@extends('layouts.public')
 
 @section('title', 'Catalogo noleggi')
 
@@ -7,5 +7,35 @@
 @endsection
 
 @section('content')
+    <div class="container">
+        <aside class="lateral-bar">
+            <h2> Categorie </h2>
+            @foreach ($topCategories as $category)
+                <a href="{{ route('catalog2', [$category->catId]) }}">{{ $category->nome }}</a>
+            @endforeach
 
+            <h2> Filtri </h2>
+            @include('layouts/filtri')
+        </aside>
+
+        <section class="catalogo">
+            <h1> Auto disponibili al noleggio </h1>
+            <div class="row justify-content-center">
+                @foreach($automobili as $auto)
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="data:image/png/jpeg;base64,{{ base64_encode($auto->foto)}}" class="card-img-top custom_card" alt="Foto Automobile">
+                            <div class="card-body">
+                                <h3 class="card-title">{{$auto->marca}}</h3>
+                                <h4 class="card-title">{{$auto->modello}}</h4>
+                                <p class="card-text">{{$auto->descrizione}}</p>
+                                <a href="{{ route('dettagli-auto', ['id' => $auto->targa])}}" class="btn btn-primary">Vai al noleggio</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            @include('paginator.paginator', ['paginator' => $automobili->withQueryString()])
+        </section>
+    </div>
 @endsection
